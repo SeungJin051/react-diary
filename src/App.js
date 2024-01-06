810b821c26acd0f8a0ba5d0e84c2cf79054a487f
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-01-06 20:36:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import "./App.css";
+import Counter from "./components/Counter";
 import MyFooter from "./components/MyFooter";
 import MyHeader from "./components/MyHeader";
 
 function App() {
-  let name = "임승진  ";
-  const number = 5;
   return (
     <div className="App">
       <MyHeader />
-      <h2
-        style={{
-          backgroundColor: "red",
-        }}
-      >
-        Hello react {name}
-      </h2>
-      <b id="bold_text">
-        {number}는 : {number % 2 === 0 ? "짝" : "홀"}
-      </b>
+      <Counter />
       <MyFooter />
     </div>
   );
