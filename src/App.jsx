2ecf00e8d6e8dc7a3fc270d83e2c5e38d21f9bbd
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-03-17 21:16:07 +0900

    복습)Props로 데이터 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
   return (
     <>
-      <h1>Hello React</h1>
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <div>자식요소 props</div>
+      </Button>
     </>
   );
 }
