27ee6c2e44eb88a9dbc516818a36a2d7594a7e88
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-04-17 23:54:11 +0900

    react-router-dom 라우팅 설정하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; // 1. "/" : 모든 일기를 조회하는 페이지
+import New from "./pages/New"; // 2. "/new" : 새로운 일기를 작성하는 페이지
+import Diary from "./pages/Diary"; // 3. "/diary" : 일기를 상세히 조회하는 페이지
+import Notfound from "./pages/Notfound";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    // Routes 태그 안에는 Route 컴포넌트만 사용 가능, 밖에는 어떤 페이지든 간에 랜더링
+    <>
+      <div>Hello</div>
+      <Routes>
+        {/* path="주소", element={컴포넌트} | *은 path에 없는 페이지일 때 (와일드카드) */}
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // 브라우저의 현재 주소를 저장하고 감시하는 BrowserRouter
+  // 리액트 개발자 도구에서 State를 통해 다양한 정보를 보관하고 있다.
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
